b266e76c3b8a4513ae621348ba1f17a66d752313
Kracav4ik <dikama2013@yandex.ru>
2016-08-18 06:41:52 +0300

    anims list

--- src/editor_window.h
+++ src/editor_window.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QtCore/QDir>
 
 #include "ui_editor.h"
 
@@ -10,6 +11,19 @@ public:
     EditorWindow() {
         setupUi(this);
 
+        load_anims();
+
         show(); 
+    }
+
+    void load_anims() {
+        QString path = QCoreApplication::applicationDirPath();
+        QDir dir(path + "/../data/anims");
+        QStringList list = dir.entryList();
+
+        for (int i = 2; i < list.size();i++) {
+            QString str = list[i];
+            animationList->addItem(str.left(str.size() - 4));
+        }
     }
 };

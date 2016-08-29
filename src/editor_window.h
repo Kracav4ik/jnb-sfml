77f32fb991c910721652ed082db1f0db41e02407
Kracav4ik <dikama2013@yandex.ru>
2016-08-29 03:01:15 +0300

    animation working(BIG RABBIT RUN)

--- src/editor_window.h
+++ src/editor_window.h
@@ -2,10 +2,12 @@
 
 #include <stdio.h>
 #include <QMainWindow>
-#include <QtCore/QDir>
+#include <QDir>
 #include <QGraphicsTextItem>
+#include <QTimer>
 
 #include "ui_editor.h"
+#include "debug.h"
 #include "frame_animation.h"
 #include "texture_manager.h"
 
@@ -16,24 +18,33 @@ private:
     QGraphicsScene scene;
     QGraphicsItem* item;
     AnimInfo animInfo;
+    QTimer animTimer;
+    bool animRuning;
+
 public:
-    EditorWindow() : anims_root(QCoreApplication::applicationDirPath() + "/../data/anims/") {
+    EditorWindow() : anims_root(QCoreApplication::applicationDirPath() + "/../data/anims/"), item(NULL), animRuning(false) {
         setupUi(this);
+
+        animTimer.setInterval(200);
 
         load_anims();
 
-        item = scene.addText("Just do it!");
+//        item = scene.addText("Just do it!");
 
         graphicsView->setScene(&scene);
-        graphicsView->scale(4, 4);
+        graphicsView->scale(14, 14);
+
+        connect(&animTimer, SIGNAL(timeout()), this, SLOT(anim_step()));
 
         show();
     }
 
     void refresh_anim() {
         frameNumber->setMaximum(animInfo._frames.size() - 1);
-        scene.removeItem(item);
-        delete item;
+        if(item){
+            scene.removeItem(item);
+            delete item;
+        }
         Frame& frame = animInfo._frames[frameNumber->value()];
         int w = frame._w;
         int h = frame._h;
@@ -64,16 +75,42 @@ public:
     }
 
 public slots:
-    void on_startAnimation_clicked() {
-        printf("Start\n");
+    void on_startAnimation_clicked(bool checked) {
+        animRuning = checked;
+    }
+
+    void on_startAnimation_toggled(bool checked) {
+        if(checked){
+            startAnimation->setText("Stop");
+            animTimer.start();
+            log("Check\n");
+        } else{
+            startAnimation->setText("Start");
+            animTimer.stop();
+            log("&Mate\n");
+        }
+    }
+
+    void anim_step() {
+        frameNumber->setValue((frameNumber->value()+1)%animInfo._frames.size());
+        log("Work\n");
     }
 
     void on_animationList_currentTextChanged(const QString& string) {
-        printf("%s\n", string.toUtf8().constData());
+        log("%s\n", string.toUtf8().constData());
         QString path = anims_root + string + ".txt";
         animInfo.load(FilePath(path.toUtf8().constData()));
 
+        bool has_animation = animInfo._frames.size() != 1;
+        startAnimation->setChecked(has_animation && animRuning);
+        startAnimation->setEnabled(has_animation);
+
+
         refresh_anim();
+    }
+
+    void on_speed_valueChanged(double value){
+        animTimer.setInterval(int(200/value));
     }
 
     void on_frameNumber_valueChanged(int){
